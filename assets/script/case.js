b905c060df8ba3717f134e0fe3459751eaf674d9
liaoyuesheng <liao_yuesheng@163.com>
2017-06-26 19:56:22 +0800

    20170626

--- assets/script/case.js
+++ assets/script/case.js
@@ -7,12 +7,16 @@ $(function(){
 			  $('.showw').on('mouseover',function(){
 			  	
 			  	//防止动画 重叠
-			  	$(this).children('div').stop().css('display','block');
+			  	$(this).children('div').stop().animate({opacity:'0.7'},600);
+			  	$(this).find('p').stop().animate({opacity:'1'},600);
+			  	$(this).find('i').stop().animate({opacity:'1'},600);
 			  })
 			  
 			  $('.showw').on('mouseout',function(){
 			  	
-			  	$(this).children('div').stop().css('display','none');
+			  	$(this).children('div').stop().animate({opacity:'0'},600);
+			  	$(this).find('p').stop().animate({opacity:'0'},600);
+			  	$(this).find('i').stop().animate({opacity:'0'},600);
 			  })
 			   
 			
